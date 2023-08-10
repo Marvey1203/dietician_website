79d86c453eeb7677e13b2eb3885aaf2ebc6bea66
Marvey1203 <102023269+Marvey1203@users.noreply.github.com>
2023-08-10 18:23:14 +0200

    Fixed seo problem

--- app/components/NavPopUp.jsx
+++ app/components/NavPopUp.jsx
@@ -3,11 +3,11 @@ import Link from "next/link";
 const NavPopUp = ({navItems, handleClose}) => {
     return ( 
         <div className="w-full h-full absolute top-0 bottom-o left-0 right-0 bg-white z-50">
-            <button onClick={handleClose}>
+            <div onClick={handleClose}>
                 <svg className="w-8 m-7" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"></path>
                 </svg>
-            </button>
+            </div>
             
             <div className="flex flex-col gap-5 justify-center items-center">
             {
