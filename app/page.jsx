af19af78381248e8facd17ab5b6b50d20cc19054
Marvey1203 <102023269+Marvey1203@users.noreply.github.com>
2023-08-10 13:42:07 +0200

    Added lazy loading

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,20 @@
-import HealthNutricion from "./components/HealthNutricion";
+//import HealthNutricion from "./components/HealthNutricion";
 import Hero from "./components/Hero";
-import ContactSection from "./components/ContactSection";
-import InvestmentSection from "./components/InvestmentSection";
-import DietitianSection from "./components/DietitianSection";
-import AboutMe from "./components/AboutMe";
-import MissionStatement from "./components/MissionStatement";
-import MoreAbout from "./components/MoreAbout";
+//import ContactSection from "./components/ContactSection";
+//import InvestmentSection from "./components/InvestmentSection";
+//import DietitianSection from "./components/DietitianSection";
+//import AboutMe from "./components/AboutMe";
+//import MissionStatement from "./components/MissionStatement";
+//import MoreAbout from "./components/MoreAbout";
+import dynamic from "next/dynamic";
 
+const HealthNutricion = dynamic(() => import("./components/HealthNutricion"))
+const InvestmentSection = dynamic(() => import("./components/InvestmentSection"))
+const DietitianSection = dynamic(() => import("./components/DietitianSection"))
+const AboutMe = dynamic(() => import("./components/AboutMe"))
+const MissionStatement = dynamic(() => import("./components/MissionStatement"))
+const MoreAbout = dynamic(() => import("./components/MoreAbout"))
+const ContactSection = dynamic(() => import("./components/ContactSection"))
 
 export default function Home() {
   return (
