c0812e185f06cffe2d235030fcc8403e0fa228aa
Marvey1203 <102023269+Marvey1203@users.noreply.github.com>
2023-08-13 15:28:04 +0200

    Tried adding meta

--- app/layout.js
+++ app/layout.js
@@ -9,11 +9,13 @@ const Footer = dynamic(() => import("@/app/components/Footer"))
 export const metadata = {
   title: 'Clinical Dietitian',
   description: 'Private practice by Tanelle Schutte',
+  
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <meta name="google-site-verification" content="FtA9RgqRmhz_iXq28yU92M3fuNjF07Cx61cFQtEoBCo" />
       <body className="text-primary">
       <div className="flex flex-col justify-center items-center">
         <div className="w-4/5">
--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,5 @@
 import Hero from "./components/Hero";
 import dynamic from "next/dynamic";
-import Head from "next/head";
 
 const HealthNutricion = dynamic(() => import("./components/HealthNutricion"))
 const InvestmentSection = dynamic(() => import("./components/InvestmentSection"))
@@ -12,9 +11,9 @@ const ContactSection = dynamic(() => import("./components/ContactSection"))
 
 export default function Home() {
   return (
-    <><Head>
-        <meta name="google-site-verification" content="FtA9RgqRmhz_iXq28yU92M3fuNjF07Cx61cFQtEoBCo" />
-    </Head>
+
+        
+
     <div className="w-full flex flex-col">
         <Hero />
         <HealthNutricion />
@@ -24,6 +23,6 @@ export default function Home() {
         <MissionStatement />
         <MoreAbout />
         <ContactSection />
-      </div></>
+      </div>
   )
 }
