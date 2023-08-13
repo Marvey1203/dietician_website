2946f9f4e11a8764df055641a32e8a9d521957a8
Marvey1203 <102023269+Marvey1203@users.noreply.github.com>
2023-08-13 15:22:31 +0200

    Adding google search console

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,6 @@
 import Hero from "./components/Hero";
 import dynamic from "next/dynamic";
+import Head from "next/head";
 
 const HealthNutricion = dynamic(() => import("./components/HealthNutricion"))
 const InvestmentSection = dynamic(() => import("./components/InvestmentSection"))
@@ -11,15 +12,18 @@ const ContactSection = dynamic(() => import("./components/ContactSection"))
 
 export default function Home() {
   return (
+    <><Head>
+        <meta name="google-site-verification" content="FtA9RgqRmhz_iXq28yU92M3fuNjF07Cx61cFQtEoBCo" />
+    </Head>
     <div className="w-full flex flex-col">
-      <Hero/>
-      <HealthNutricion/>
-      <InvestmentSection/>
-      <DietitianSection/>
-      <AboutMe/>
-      <MissionStatement/>
-      <MoreAbout/>
-      <ContactSection/>
-    </div>
+        <Hero />
+        <HealthNutricion />
+        <InvestmentSection />
+        <DietitianSection />
+        <AboutMe />
+        <MissionStatement />
+        <MoreAbout />
+        <ContactSection />
+      </div></>
   )
 }
