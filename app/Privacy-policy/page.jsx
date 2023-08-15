3ca97fee6aef8cf563af2f0b10d7f396e34a458a
Marvey1203 <102023269+Marvey1203@users.noreply.github.com>
2023-08-15 22:51:45 +0200

    Finished the privacy policy and cookie policy

--- app/Privacy-policy/page.jsx
+++ app/Privacy-policy/page.jsx
@@ -1,7 +1,87 @@
 const PrivacyPolicy = () => {
     return ( 
-        <div>
-            
+        <div className="w-3/4 flex flex-col justify-start mx-auto gap-5">
+            <h5 className="font-bold text-3xl py-3">Privacy Policy</h5>
+            <p>This Privacy Policy outlines how Tanelle Schutte ("the Dietitian," "we," "us," or "our") collects, uses, discloses, and protects the personal information of users ("you" or "your") who access and use our website. We are committed to ensuring the privacy and security of your personal information. By using our website, you consent to the practices described in this Privacy Policy.</p>
+            <h6 className="font-semibold text-2xl py-2">1. Information We Collect:</h6>
+            <div className="pl-5">
+                <p><span className="font-bold px-2">• Name:</span>We collect your name to identify you and facilitate appointment scheduling.</p>
+                <p><span className="font-bold px-2">• Email Address:</span> We collect your email address to communicate with you regarding your appointment requests, confirmations, and any other related information.</p>
+                <p><span className="font-bold px-2">• Phone Number:</span>We collect your phone number to contact you in case of any changes to your appointment and for appointment-related communication.</p>
+            </div>
+            <h6 className="font-semibold text-2xl py-2">2. Use of Personal Information:</h6>
+            <div className="pl-5">
+                <p><span className="font-bold px-2">•</span>To schedule, confirm, and manage your appointments.</p>
+                <p><span className="font-bold px-2">•</span>To communicate with you regarding your appointments and related matters.</p>
+                <p><span className="font-bold px-2">•</span>To respond to your inquiries and provide customer support.</p>
+                <p><span className="font-bold px-2">•</span>To improve our services and enhance user experience.</p>
+            </div>
+            <h6 className="font-semibold text-2xl py-2">3. Disclosure of Personal Information:</h6>            
+            <p>We may disclose your personal information in the following circumstances:</p>
+            <div className="pl-5">
+                <p><span className="font-bold px-2">•</span>To third-party service providers who assist us in appointment scheduling and communication.</p>
+                <p><span className="font-bold px-2">•</span>If required by law, regulation, or legal process.</p>
+                <p><span className="font-bold px-2">•</span>To protect our rights, privacy, safety, or property, and that of our users and the public.</p>
+                <p><span className="font-bold px-2">•</span>In connection with a merger, acquisition, or sale of all or a portion of our assets.</p>
+            </div>
+            <h6 className="font-semibold text-2xl py-2">4. Data Security:</h6>
+            <p>
+                We implement reasonable security measures to protect your personal information from unauthorized access, disclosure, alteration, or destruction. However, no method of transmission over the internet or electronic storage is completely secure. Therefore, while we strive to protect your personal information, we cannot guarantee its absolute security.
+            </p>
+            <h6 className="font-semibold text-2xl py-2">5. Retention of Personal Information:</h6>
+            <p>
+                We retain your personal information only for as long as necessary to fulfill the purposes for which it was collected and to comply with legal obligations.            
+            </p>
+            <h6 className="font-semibold text-2xl py-2">6. Your Rights:</h6>            
+            <p>You have the right to:</p>
+            <div className="pl-5">
+                <p><span className="font-bold px-2">•</span>Access the personal information we hold about you.</p>
+                <p><span className="font-bold px-2">•</span>Correct inaccuracies in your personal information.</p>
+                <p><span className="font-bold px-2">•</span>Withdraw your consent at any time.</p>
+                <p><span className="font-bold px-2">•</span>Delete your personal information, subject to legal requirements.</p>
+                <p><span className="font-bold px-2">•</span>Lodge a complaint with a relevant data protection authority.</p>
+            </div>
+            <h6 className="font-semibold text-2xl py-2">7. Children's Privacy:</h6> 
+            <p>
+                Our website is not directed at children under the age of 13. We do not knowingly collect personal information from children. If you believe we have collected personal information from a child under 13, please contact us immediately.
+            </p>            
+            <h6 className="font-semibold text-2xl py-2">8. Changes to the Privacy Policy:</h6> 
+            <p>
+                We may update this Privacy Policy from time to time. Any changes will be posted on this page, and the revised date will be indicated at the top of the Privacy Policy.
+            </p>            
+            <h6 className="font-semibold text-2xl py-2">9. Contact Us:</h6> 
+            <p>
+                If you have any questions or concerns regarding this Privacy Policy or the use of your personal information, please contact us at <a  className="underline text-bold" href="mailto:tanelleschutte@gmail.com">tanelleschutte@gmail.com.</a>
+                <br /><br />
+                By using our website, you acknowledge that you have read, understood, and agreed to this Privacy Policy. If you do not agree with any part of this policy, please refrain from using the website.
+            </p>
+            <h5 className="font-bold text-3xl py-3">Cookie Policy</h5>
+            <p>
+                This Cookie Policy explains the use of cookies on Tanelle Schutte's ("the Dietitian," "we," "us," or "our") website. Please read this policy to understand how we use cookies, although no cookies are currently in use on our website.            </p>  
+                <h6 className="font-semibold text-2xl py-2">1. What Are Cookies:</h6> 
+            <p>
+                Cookies are small text files that are placed on your device (computer, tablet, or mobile) when you visit a website. They can help websites remember your preferences, improve your user experience, and gather information about your interactions with the website.            
+            </p>            
+            <h6 className="font-semibold text-2xl py-2">2. Our Use of Cookies:</h6> 
+            <p>
+                As of the date of this policy, we do not use any cookies on our website. Therefore, we do not collect any information through cookies about your browsing behavior or preferences.
+            </p>            
+            <h6 className="font-semibold text-2xl py-2">3. Managing Cookies:</h6> 
+            <p>
+                Since we do not use cookies, there is no need for you to manage or control any cookies related to our website. You can continue to use our website without concerns about cookie management.            
+            </p>              
+            <h6 className="font-semibold text-2xl py-2">4. Changes to This Policy:</h6> 
+            <p>
+                In the future, if we decide to use cookies on our website, we will update this Cookie Policy accordingly to provide information about the types of cookies we use, their purposes, and how you can manage them.
+            </p>              
+            <h6 className="font-semibold text-2xl py-2">5. Contact Us:</h6> 
+            <p>
+                If you have any questions or concerns regarding this Cookie Policy or the use of cookies on our website, please contact us at <a className="underline text-bold" href="mailto:tanelleschutte@gmail.com">tanelleschutte@gmail.com.</a>
+                <br /><br />
+                Please note that this policy is subject to change, and any updates will be posted on this page.
+                <br /><br />
+                By using our website, you acknowledge that you have read, understood, and agreed to the information provided in this Cookie Policy. If you have any reservations about cookies, please refrain from using the website.
+            </p>        
         </div>
      );
 }
